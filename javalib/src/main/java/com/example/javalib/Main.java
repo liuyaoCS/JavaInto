4cb6d34c98e26c3a8c959e412f3e953bb3226a62
liuyaoCS <liuyao@chinaso.com>
2018-04-21 16:46:51 +0800

    init commit

---
+++ javalib/src/main/java/com/example/javalib/Main.java
@@ -0,0 +1,30 @@
+package com.example.javalib;
+
+public class Main {
+    public static void main(String[] args){
+        testProperties();
+    }
+
+    private static void testProperties() {
+        String path=System.getProperty("java.class.path");
+        String[] paths=path.split(":");
+        for (String p:paths) {
+            System.out.println(p);
+        }
+
+        String ext=System.getProperty("java.ext.dirs");
+        System.out.println(ext);
+
+        String os=System.getProperty("os.name");
+        System.out.println(os);
+
+        java.util.Properties props = System.getProperties();
+        java.util.Enumeration keys = props.keys();
+        String key = null;
+        while(keys.hasMoreElements()){
+            key = keys.nextElement().toString();
+            System.out.println(key + "=" + props.get(key));
+        }
+    }
+
+}
