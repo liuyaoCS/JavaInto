49cb62c46a4af3dc2eee0a1c06611a8edde7f01a
liuyao-s <liuyao-s@360.cn>
2018-04-23 14:19:38 +0800

    config .gitignore

--- javalib/src/main/java/com/example/javalib/classloader/SystemProperties.java
+++ javalib/src/main/java/com/example/javalib/classloader/SystemProperties.java
@@ -6,17 +6,17 @@ public class SystemProperties {
     }
 
     private static void testProperties() {
-        String path=System.getProperty("java.class.path");
-        String[] paths=path.split(":");
-        for (String p:paths) {
-            System.out.println(p);
-        }
-
-        String ext=System.getProperty("java.ext.dirs");
-        System.out.println(ext);
-
-        String os=System.getProperty("os.name");
-        System.out.println(os);
+//        String path=System.getProperty("java.class.path");
+//        String[] paths=path.split(":");
+//        for (String p:paths) {
+//            System.out.println(p);
+//        }
+//
+//        String ext=System.getProperty("java.ext.dirs");
+//        System.out.println(ext);
+//
+//        String os=System.getProperty("os.name");
+//        System.out.println(os);
 
         java.util.Properties props = System.getProperties();
         java.util.Enumeration keys = props.keys();
